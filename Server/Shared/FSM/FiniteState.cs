2dd2ab8c8b3d0587505b328bdb3152b5c370b0c5
YiDoSeon <lds3794@gmail.com>
2026-02-10 13:16:55 +0900

    feat(fsm): FSM 추가

---
+++ Server/Shared/FSM/FiniteState.cs
@@ -0,0 +1,19 @@
+namespace Shared.FSM
+{
+    public abstract class FiniteState<T, TEnum> where TEnum : System.Enum
+    {
+        protected T entity;
+        protected StateMachine<T, TEnum> stateMachine;
+
+        public FiniteState(T entity, StateMachine<T, TEnum> stateMachine)
+        {
+            this.entity = entity;
+            this.stateMachine = stateMachine;
+        }
+
+        public abstract void Enter();
+        public abstract void Update();
+        public abstract void FixedUpdate();
+        public abstract void Exit();
+    }    
+}
